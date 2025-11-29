a475c3fca01c6c9bfcb186c0926c2e595d0d16c2
jw <choi.jae100@gmail.com>
2025-11-29 05:53:32 -0800

    update column names

--- schema.sql
+++ schema.sql
@@ -1,81 +1,80 @@
 -- drop Table IF EXISTS decks;
 -- drop Table IF EXISTS cards;
 -- drop Table IF EXISTS deck_cards;
+DROP TABLE IF EXISTS decks;
+DROP TABLE IF EXISTS cards;
+DROP TABLE IF EXISTS deck_cards;
 
-Create Table if not exists user_info (
-    id INTEGER PRIMARY KEY,
-    sessionid TEXT NOT NULL,
-    csrfToken TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS user_info
+(
+    id         INTEGER PRIMARY KEY,
+    session_id TEXT NOT NULL,
+    csrf_token TEXT NOT NULL,
+    added_at   TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS decks
+(
+    id       INTEGER PRIMARY KEY,
+    name     TEXT NOT NULL,
+    source   TEXT NOT NULL,
+    url      TEXT NOT NULL,
     added_at TEXT NOT NULL
 );
 
--- Table for Decks
-CREATE TABLE IF NOT EXISTS decks (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    source TEXT NOT NULL,
-    url TEXT NOT NULL,
-    added_at TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS cards
+(
+    id            INTEGER PRIMARY KEY,
+    name          TEXT NOT NULL,
+    printed_name  TEXT,
+    flavor_name   TEXT,
+    face_name     TEXT,
+    mana_cost     TEXT,
+    mana_value    REAL,
+    power         TEXT,
+    original_text TEXT,
+    type          TEXT,
+    types         TEXT,
+    mtg_arena_id  TEXT,
+    scryfall_id   TEXT,
+    availability  TEXT,
+    colors        TEXT,
+    keywords      TEXT
 );
 
-
-CREATE TABLE IF NOT EXISTS cards (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    printedName TEXT,
-    flavorName TEXT,
-    faceName TEXT
-    manaCost TEXT,
-    manaValue REAL,
-    power TEXT,
-    originalText TEXT,
-    type TEXT,
-    types TEXT,
-    mtgArenaId TEXT,
-    scryfallId TEXT,
-    availability TEXT,
-    colors TEXT,
-    keywords TEXT
-);
-
--- Junction table for Cards in Decks
-CREATE TABLE IF NOT EXISTS deck_cards (
-    id INTEGER PRIMARY KEY,
-    deck_id INTEGER NOT NULL,
-    card_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS deck_cards
+(
+    id       INTEGER PRIMARY KEY,
+    deck_id  INTEGER NOT NULL,
+    card_id  INTEGER NOT NULL,
     quantity INTEGER NOT NULL,
     FOREIGN KEY (deck_id) REFERENCES decks (id) ON DELETE CASCADE,
     FOREIGN KEY (card_id) REFERENCES cards (id),
     UNIQUE (deck_id, card_id)
 );
 
-
-
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
-
-
-ATTACH DATABASE 'AllPrintings.sqlite' AS ap;
-       
-
--- Only insert cards if the cards table is empty
-INSERT OR IGNORE INTO cards (name, printedName, flavorName, manaCost, manaValue, power, originalText, type, types, mtgArenaId, scryfallId, availability, colors, keywords)
-SELECT 
-    c.name,
-    c.printedName,
-    c.flavorName,
-    c.manaCost,
-    c.manaValue,
-    c.power,
-    c.originalText,
-    c.type,
-    c.types,
-    ci.mtgArenaId,
-    ci.scryfallId,
-    c.availability,
-    c.colors,
-    c.keywords
-FROM ap.cards c
-INNER JOIN ap.cardIdentifiers ci ON c.uuid = ci.uuid
-WHERE c.availability LIKE '%arena%';
+-- 
+-- ATTACH DATABASE 'AllPrintings.sqlite' AS ap;
+-- 
+-- INSERT OR IGNORE INTO cards (name, printed_name, flavor_name, mana_cost, mana_value, power, original_text, type, types,
+--                              mtg_arena_id, scryfall_id, availability, colors, keywords)
+-- SELECT c.name,
+--        c.printedName,
+--        c.flavorName,
+--        c.manaCost,
+--        c.manaValue,
+--        c.power,
+--        c.originalText,
+--        c.type,
+--        c.types,
+--        ci.mtgArenaId,
+--        ci.scryfallId,
+--        c.availability,
+--        c.colors,
+--        c.keywords
+-- FROM ap.cards c
+--          INNER JOIN ap.cardIdentifiers ci ON c.uuid = ci.uuid
+-- WHERE c.availability LIKE '%arena%';
